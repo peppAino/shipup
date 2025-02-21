c3e663bb54c4037b021233ea8e8242046191454b
peppAino <erpeppe@tin.it>
2025-02-21 11:02:26 +0100

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-// Configurazione per ShipUp Blog
-
-const CONFIG = {
-    supabaseUrl: 'https://amxtzqdawysnqpjnsgic.supabase.co',
-    supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFteHR6cWRhd3lzbnFwam5zZ2ljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2MTYwNDQyMjQsImV4cCI6MTk3MjYyMDIyNH0.tOg5-XaN6xJkL2U9n4D3dB2e3-cQwG0hzgU1xI5lq8g',
-    encryptedAdminPassword: 'U2FsdGVkX19vD8qZc3Jm7uP4hgC9Qv5gm68f1u8pZDo=', // Password admin criptata
-    encryptionKey: 'secret-key' // Chiave per la crittografia
-};
